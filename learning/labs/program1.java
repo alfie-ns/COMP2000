091c0d5bb6fa344ce0c35f321250df4af049f4e2
Alfie Nurse <oladeanio@Alfies-MacBook-Pro.local>
2024-09-26 01:21:12 +0100

    Minor: Java print 'hello world'

--- learning/labs/program1.java
+++ learning/labs/program1.java
@@ -3,6 +3,7 @@ package year2_labs;
 public class program1 {
     public static void main(String[] args) {
         System.out.println("Test program");
+        System.out.println("Hello World");
     }
     
 }
